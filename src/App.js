4d89f5d83930a525ebb2c13e39a7dc8bb15de699
Chang Nguyen <chang@canary-software.com>
2023-02-22 14:52:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {path: '/', element: <Home />},
+//   {path: '/products', element: <Products />}
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>My home page</h1>
+}
+
+export default Home;
